9913a3b89b1875fa28e325622bb46737fbaff9cd
Prince <prince@Princes-MacBook-Pro.local>
2025-11-16 01:10:23 +0530

    Updated

--- src/components/contact/ContactUs.tsx
+++ src/components/contact/ContactUs.tsx
@@ -6,13 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
 import { schoolInfo } from "@/mock/mock";
 
